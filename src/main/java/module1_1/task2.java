6bab00d2043b81225401af42fe0baee81f50da69
onnik <onni.kivinen@gmail.com>
2024-07-16 21:40:16 +0300

    added comment

--- src/main/java/module1_1/task2.java
+++ src/main/java/module1_1/task2.java
@@ -1,14 +1,17 @@
 package module1_1;
 
+/*    4-rivin kolmio for looppi
+            *
+           ***
+          *****
+         *******             */
 public class task2 {
 
     public static void main(String[] args) {
-        // 4-rivin kolmio for looppi
+
         for (int i = 0; i <= 3; i++) {
             System.out.println(" ".repeat((4 - i)) + "*".repeat(1 + i) + "*".repeat(i));
         }
 
     }
 }
-
-
