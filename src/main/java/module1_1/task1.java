2421e5a12934f69bf52dddcd3672bfde082b8426
onnik <onni.kivinen@gmail.com>
2024-07-16 18:27:40 +0300

    first commit

---
+++ src/main/java/module1_1/task1.java
@@ -0,0 +1,7 @@
+package module1_1;
+
+public class task1 {
+    public static void main(String[] args) {
+        System.out.print("Moro, JavaUkko");
+    }
+}
---
+++ src/main/java/module1_1/task2.java
@@ -0,0 +1,13 @@
+package module1_1;
+
+public class task2 {
+    // aika jonne tapa tehä oon nobo oo hiljaa NI
+    public static void main(String[] args) {
+        String space = " ";
+        String aster = "*";
+        System.out.println(space.repeat(4) + aster);
+        System.out.println(space.repeat(3) + aster.repeat(3));
+        System.out.println(space.repeat(2) + aster.repeat(5));
+        System.out.println(space.repeat(1) + aster.repeat(7));
+    }
+}
