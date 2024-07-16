6f8edfad70bd9f3f27a3c1b2866cbf79fbf96e29
onnik <onni.kivinen@gmail.com>
2024-07-16 18:43:01 +0300

    chore: Add task3.java file for module1_1

---
+++ src/main/java/module1_1/task3.java
@@ -0,0 +1,31 @@
+package module1_1;
+
+import java.util.Scanner;
+
+public class task3 {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("Give the first number:");
+            int first = Integer.parseInt(scanner.nextLine());
+
+            System.out.println("Give the second number:");
+            int second = Integer.parseInt(scanner.nextLine());
+
+            System.out.println("Give the third number:");
+            int third = Integer.parseInt(scanner.nextLine());
+
+            int sum = first + second + third;
+            int product = first * second * third;
+            float average = sum / 3;
+
+            System.out.println("\nThe outcome of your numbers");
+            System.out.println("Sum: " + (sum));
+            System.out.println("Product: " + (product));
+            System.out.println("Average: " + (average));
+
+        }
+    }
+
+}
